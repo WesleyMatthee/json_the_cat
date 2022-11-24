3a16a4c64941780a1828186cd604ed468f6eef9b
WesleyMatthee <wrmatthee@gmail.com>
2022-11-24 21:16:09 +0000

    refactored code and installed Mocha and Chai for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,31 @@
 const request = require("request");
 //console.log(request);
 
-/*
-The particular endpoint that will allow us to search breed information is: https://api.thecatapi.com/v1/breeds/search.
-
-The documentation page for it is: https://docs.thecatapi.com/api-reference/breeds/breeds-search
-*/
-const breed = process.argv[2];
 
 
-//get the request lib from npm
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (error) {
-    console.log(error);
-    return;
-  }
+  //get the request lib from npm
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
 
-  const data = JSON.parse(body); //turns object into a string
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback('Server responded with:' + response.statusCode, null);
+      return;
+    }
+    const data = JSON.parse(body); //turns object into a string
 
-  if (data.length === 0) {
-    console.log("This breed does not exist");
-    return;
-  }
+    if (data.length === 0) {
+      callback("This breed does not exist");
+      return;
+    }
 
-  console.log(data[0].description);
-  //console.log(typeof data);
-});
+    callback(data[0].description);
+    //console.log(typeof data);
+  });
+};
 
+module.exports = { fetchBreedDescription };
